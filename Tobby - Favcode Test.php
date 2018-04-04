aadf121a5520a74b12f0f251a955436ba2a6e9d0
Tobby Junior <32496239+TobbyJay@users.noreply.github.com>
2018-04-03 17:06:29 -0700

    renamed
    renamed extension from html to php

---
+++ Tobby - Favcode Test.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en"><head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+	<meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Tobby - FavCode Test</title>
+
+	<meta name="description" content="African development through partnership">
+
+
+	<!-- Open Graph data -->
+	<meta property="og:title" content="Submission for Felix - Favcode ">
+	<meta property="og:type" content="article">
+	<meta property="og:image" content="Felix%20-%20Favcoder_files/art.png">
+	<meta property="og:description" content="African development through partnership">
+
+	<!-- Open Graph data <link href="Felix%20-%20Favcoder_files/css.css" rel="stylesheet" type="text/css">-->
+
+<link rel="icon" href="images/download (1).png">
+<link rel="stylesheet" href="Stylesheets/stylesheet.css" type="text/css">
+	<link rel="stylesheet" href="Stylesheets/style.css" type="text/css">
+<link rel="stylesheet" href="Stylesheets/new4.css" type="text/css">
+<link rel="stylesheet" href="Stylesheets/tobby.css" type="text/css">
+<head>
+</head>
+
+
+<body  >
+
+
+<div class="container">
+<!--Create Header -->
+
+<header class="header" >
+  <img src="images/favcode54-logo-white2.png" alt="favcode"  id="img2">
+</header>
+
+
+<!--div for Intro -->
+
+    <div class="">
+      <h1> I'm Umoh Sunday Tobby <span class="for">and I am a </span><br/>
+	  Future Favcoder.</h1>
+	  
+	  <!--div for my pishure -->
+	    <div class="profile_pic" style="height:250px;">
+	  <img src="images/32496239.jpg" alt="tobby_pic" style="height:200px;" style="width:260px"  class="img1">  
+	  </div>
+	  
+	  <!--div for Mission -->
+	  <div>
+      <h2>My mission for Favcode is to: </h2>
+	  <p class="meta"> Build and develop useful,classy but simple and well documented Websites and Web-apps with easy to understand User-Interface that will allow the Users to have an awesome User-experience.</p>
+    </div>
+	  <br/>
+	   <div>
+	   <hr>
+	   
+	   <!--div for why i should be chosen and link to my bio -->
+	   <div class="">
+      <h2  class="meta">This is why you should choose me.</h2>
+     
+	 <p class="meta"><b>Passion,</b>becoming a Web/Software developer has always been my aspiration,and due to this im willing to do all i can in order to achieve this,i'm a <b>Good Listener </b>and i love follow instructions to the latter,i dislike laziness as i am <b>not Lazy</b> as i am always ready to learn,im also a <b>Problem Solver</b>, a requirement i believe all developers should have,plus im a <b>deep thinker</b> and <b>Lover Of Art</b>,now combining art and coding will really help in building a beautiful website/web-app that users will love and be able to easily interact with,now thats the beauty and joy of going into web development and being a Web Developer too.<b>And i would like to be Brutally Honest,</b>when it comes to ones passion and love for something, i believe one shouldn't pursue it because of the Money or currency involved,and this is same with me today,i believe passion beats currency,surprisingly i pick passion over money which might seem funny,i'm also a <b>Good communicator</b> and i love being in a team because i know i will learn a lot from people. Lastly, i believe with the little knowledge i have so far which is still a basic knowledge of <b>HTML and CSS</b> can be turned into Gold and Nurtured based on the Experience that i will gain if i'm being accepted for this post of an Entry Level Web Developer. </p>
+	 <h4>Thank You.</h4> 
+    <hr>
+	 
+	
+	
+				<h5 class="meta">Here is my <a class ="visited " href="https://www.linkedin.com/in/tobby-junior-26115a13b/"> Linkedln Bio Page.</a>
+				and also my<a class ="visited " href="https://github.com/TobbyJay"> Github Bio Page.</a></h5>
+	</div>
+	
+	   
+	   
+      
+    
+  
+    
+    
+  </div>
+  
+  
+
+</div>
+
+ <!--Footer -->
+<footer  class="">
+			<p>April 1, 2018. Submission for Felix - Favcode Partnership  <br/>
+			Made by Tobby Junior.</p>
+		</footer>
+
+</body>
+</html>
